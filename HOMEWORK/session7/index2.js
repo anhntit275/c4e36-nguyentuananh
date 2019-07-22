dae26e914266cb5478ac880778322e1fbb33e2c8
anhntit275 <anhntit275@gmail.com>
2019-07-22 12:57:44 +0700

    homework

--- HOMEWORK/session7/index2.js
+++ HOMEWORK/session7/index2.js
@@ -4,9 +4,9 @@
 let dropdownlistColor = document.getElementById('dropdownlist-color');
 let btnRemove = document.getElementById('btn-remove');
 btnRemove.addEventListener('click', () => {
-    for (let i = 0; i < dropdownlistColor.options.length; i++) {
-        if (dropdownlistColor.options[i].selected) {
-            dropdownlistColor.remove(i);
+    for (let i = 0; i < dropdownlistColor.length; i++) {
+        if (dropdownlistColor[i].value === dropdownlistColor.value) {
+            dropdownlistColor[i].remove();
         }
         }
 })
